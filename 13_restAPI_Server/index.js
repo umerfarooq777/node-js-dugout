0cd9cc403debd54fdebb3dc18aa114c19cbce6f3
umerfarooq777 <umerfarooq03100276796@gmail.com>
2023-07-10 18:42:50 +0500

    all api done

--- 13_restAPI_Server/index.js
+++ 13_restAPI_Server/index.js
@@ -1,25 +1,76 @@
 const express = require("express");
+const fs = require("fs");
 const USERS = require("./MOCK_DATA.json");
 
 const app = express();
 const port = 8000;
 
-//======== GET
+app.use(express.urlencoded({extended:false}));
+
+
+
+
+//======== /users
 
 app.get("/users", (req, res) => {
-    const listHTML = `<ul>${USERS?.map((user)=>`<li>${user?.first_name}</li>`).join("")}</ul>`
+  const listHTML = `<ul>${USERS?.map((user)=>`<li>${user?.first_name}</li>`).join("")}</ul>`
   return res.send(listHTML);
 });
+
+
+//======== /api/users
 app.get("/api/users", (req, res) => {
   return res.json(USERS);
 });
-app.get("/api/user/:id", (req, res) => {
+
+
+//======== /api/user/:id
+app.route("/api/user/:id")
+
+  .get((req, res) => {
     const _id = Number(req.params.id);
 
     const user = USERS.find((user)=>user.id === _id);
      return res.json(user);
-});
+  })
+  .patch((req, res) => {
+    
+    const _id = Number(req.params.id);
+    const  body = req.body;
+    index = USERS.findIndex(user => user.id ===_id);
+    //  console.log(body);
+    USERS[index] = {id:_id,...body}
+    
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(USERS),(err,data)=>{
+    
+      return res.json({status:"user updated", id :_id});
+    })
+  })
+  .delete((req, res) => {
+    const _id = Number(req.params.id);
+    index = USERS.findIndex(user => user.id ===_id);
+    //  console.log(body);
+    USERS.splice(index,1);
+    
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(USERS),(err,data)=>{
+    
+      return res.json({status:"user Deleted", id :_id});
+    })
+  })
+  ;
 
+//======== /api/createUser
+
+app.post("/api/createUser", (req, res) => {
+ const  body = req.body;
+//  console.log(body);
+USERS.push({id:USERS.length+1,...body});
+
+fs.writeFile("./MOCK_DATA.json",JSON.stringify(USERS),(err,data)=>{
+
+  return res.json({status:"user created", id :USERS.length});
+})
+});
 
 
 
